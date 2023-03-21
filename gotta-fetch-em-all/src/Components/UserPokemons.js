c4a2c115884a7709f1769ed28774d9e5b663f3ee
Silvia Lupsa <lupsasilvia@gmail.com>
2023-03-21 14:30:12 +0200

    display fighters

--- gotta-fetch-em-all/src/Components/UserPokemons.js
+++ gotta-fetch-em-all/src/Components/UserPokemons.js
@@ -1,9 +1,8 @@
-const UsersPokemons = (props) => {
+const UserPokemons = (props) => {
     const svg = props.svg;
     const name = props.name
     return (
         <div>
-        <h3>Choose your fighter:</h3>
         <button>
                 <h3>{name}</h3>
                 <img src={svg} />
@@ -12,4 +11,4 @@ const UsersPokemons = (props) => {
     );
 }
 
-export default UsersPokemons;
+export default UserPokemons;
