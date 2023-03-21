b6401e05742c0f0e1cce6fc9a61b16ff5c4915ba
Silvia Lupsa <lupsasilvia@gmail.com>
2023-03-21 10:20:28 +0200

    no pokemons in area

--- gotta-fetch-em-all/src/Components/Sprite.js
+++ gotta-fetch-em-all/src/Components/Sprite.js
@@ -1,10 +1,15 @@
 
 const Sprite = (props) => {
     const svg = props.svg;
-    const name = props.name
-    return (
-      <img src={svg} />
-    
+  const name = props.name
+  const click = props.click;
+  return (<div>
+    <h3>{name}</h3>
+    <img src={svg} />
+    <button onClick={click}>Back</button>
+    </div>
+ 
+      
     //<div>Merge</div>
   );
 };
