f688c237665f7ce2c049a68465076c504db518d3
MateiMadalina <116349352+MateiMadalina@users.noreply.github.com>
2023-03-22 11:54:07 +0200

    All functionalities done

--- gotta-fetch-em-all/src/Components/Sprite.js
+++ gotta-fetch-em-all/src/Components/Sprite.js
@@ -1,11 +1,13 @@
 const Sprite = (props) => {
-    const svg = props.svg;
+  const svg = props.svg;
   const name = props.name
   const click = props.click;
   const buttonName = props.buttonName;
+  const hp = props.hp;
   return (<div>
     
     <h4>{name}</h4>
+    <h5>{hp}</h5>
     <img src={svg} />
     <button onClick={click}>{buttonName}</button>
     </div>
